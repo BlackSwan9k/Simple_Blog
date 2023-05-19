f21e48d5a99929eb48dd9bcde0f8931b78cb26b3
BlackSwan9k <pragyaharsh05@gmail.com>
2023-05-19 22:01:10 +0530

    Created home page for blogs

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- //Include bootstrap CSS CDN here-->
+        <link rel="stylesheet" type="text/css" href="{{url('assets/css/styles.css')}}"/> 
+
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+
+    </head>
+
+    <body>
+
+    @yield('content')
+
+    </body>
+    <!-- //Include bootstrap JS CDN here -->
+    <script src="{{url('assets/js/scripts.js')}}"></script>
+
+</html>
