cbd30eb7eca041731843f607ca33cdd435dfb1e9
BlackSwan9k <pragyaharsh05@gmail.com>
2023-05-19 22:30:46 +0530

    fixed some errors

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12 pt-2">
                  <div class="row">
                     <div class="col-8">
-                        <h1 class="display-one">Our Blog!</h1>
+                        <h1 class="display-one">My Blog!</h1>
                         <p>Enjoy reading our posts. Click on a post to read!</p>
                     </div>
                     <div class="col-4">
